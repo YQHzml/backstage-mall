bfe67b1a0dd550ce75522d44b5eb91d64659c2a6
YuanL <3023492630@qq.com>
2023-12-08 15:36:31 +0800

    封装拖拽组件和修改样式

---
+++ src/pages/anime/components/DraggableItem.jsx
@@ -0,0 +1,58 @@
+import React, { useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
+import styles from "../index.module.scss";
+
+const DraggableItem = ({ item, index, category, moveItem, data, setData }) => {
+  const ref = useRef(null);
+
+  const [, drop] = useDrop({
+    accept: "item",
+    canDrop: () => category !== "new",
+    drop: (itemProps, monitor) => {
+      const dragIndex = itemProps.index;
+      const hoverIndex = index;
+      const sourceCategory = itemProps.category;
+      const destCategory = category;
+
+      if (
+        dragIndex === hoverIndex &&
+        sourceCategory === destCategory &&
+        monitor.isOver({ shallow: true })
+      ) {
+        return;
+      }
+
+      moveItem(
+        sourceCategory,
+        destCategory,
+        dragIndex,
+        hoverIndex,
+        data,
+        setData
+      );
+      itemProps.index = hoverIndex;
+      itemProps.category = destCategory;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "item",
+    item: { id: item.id, index, category },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  drag(drop(ref));
+
+  return (
+    <div ref={ref} className={styles.drag_container}>
+      <div className={styles.drag_name}>
+        <img src={item.img} alt={item.name} />
+        {item.name}
+      </div>
+    </div>
+  );
+};
+
+export default DraggableItem;
--- src/pages/anime/components/anime.jsx
+++ src/pages/anime/components/anime.jsx
@@ -1,84 +1,24 @@
 import React, { useState } from "react";
 import styles from "../index.module.scss";
 import { dataArray } from "./data";
-import { useDrag, useDrop } from "react-dnd";
-
-const ItemTypes = {
-  ITEM: "item",
-};
+import DraggableItem from "./DraggableItem";
 
 function Anime() {
   const [data, setData] = useState(dataArray);
 
   const moveItem = (
-    sourceIndex,
-    targetIndex,
     sourceCategory,
-    targetCategory
+    destCategory,
+    sourceIndex,
+    destIndex,
+    data,
+    setData
   ) => {
-    const sourceList = [...data[sourceCategory]];
-    const targetList = [...data[targetCategory]];
-    const draggedItem = sourceList[sourceIndex];
-
-    sourceList.splice(sourceIndex, 1);
-    targetList.splice(targetIndex, 0, draggedItem);
-
-    setData((prevData) => ({
-      ...prevData,
-      [sourceCategory]: sourceList,
-      [targetCategory]: targetList,
-    }));
-  };
-
-  const DraggableItem = ({ item, index, category }) => {
-    const [{ isDragging }, drag] = useDrag(
-      () => ({
-        type: ItemTypes.ITEM,
-        item: { id: item.id, index, category },
-        collect: (monitor) => ({
-          isDragging: monitor.isDragging(),
-          opacity: monitor.isDragging() ? 0.5 : 1,
-        }),
-      }),
-      []
-    );
-
-    const [, drop] = useDrop(
-      () => ({
-        accept: ItemTypes.ITEM,
-        drop: (droppedItem) => {
-          if (droppedItem.category !== category) {
-            if (data[category].length === 0) {
-              // 如果列表为空，创建一个新的数组并添加拖拽的项
-              const newList = [droppedItem.item];
-              setData({
-                ...data,
-                [category]: newList,
-              });
-            } else {
-              moveItem(
-                droppedItem.index,
-                index,
-                droppedItem.category,
-                category
-              );
-            }
-          }
-        },
-      }),
-      []
-    );
-
-    if (isDragging) return null;
-
-    return (
-      <div ref={(node) => drag(drop(node))} className={styles.drag_container}>
-        <div className={styles.drag_name}>
-          <img src={item.img} alt={item.name} />
-          {item.name}
-        </div>
-      </div>
-    );
+    const itemToMove = data[sourceCategory][sourceIndex];
+    const newData = { ...data };
+    newData[sourceCategory].splice(sourceIndex, 1);
+    newData[destCategory].splice(destIndex, 0, itemToMove);
+    setData(newData);
   };
 
   return (
@@ -93,6 +33,9 @@ function Anime() {
                 item={item}
                 index={index}
                 category={category}
+                moveItem={moveItem}
+                data={data}
+                setData={setData}
               />
             ))}
           </div>
