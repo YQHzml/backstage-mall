069efa4775751e8bd6940b0101c6ce3aa77f44c7
YuanL <3023492630@qq.com>
2024-03-02 12:16:03 +0800

    fix:解决login页面重定向问题

--- src/routers/autuRoute.jsx
+++ src/routers/autuRoute.jsx
@@ -1,29 +1,32 @@
-import React from "react";
+import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+
 const allowList = ["/login", "/register"];
 const loginRoute = "/login";
 const indexRoute = "/home";
+
 export default function AuthRoute(props) {
   const location = useLocation();
   const navigate = useNavigate();
 
   const { children } = props;
 
-  if (localStorage.getItem("token")) {
-    // 有 token 的状态下禁止用户回到登录页，重定向到首页
-    if (location.pathname === loginRoute) {
-      return navigate(indexRoute);
-    } else {
-      // 其他路由均可正常跳转
-      return <>{children}</>;
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate(loginRoute);
+    } else if (location.pathname === loginRoute) {
+      navigate(indexRoute);
     }
-  } else {
-    // 无 token 的状态下，如果要跳转的路由是白名单中的路由，正常跳转
-    if (allowList.includes(location.pathname || "")) {
+  }, [location.pathname, navigate]);
+
+  if (!localStorage.getItem("token")) {
+    if (allowList.includes(location.pathname)) {
       return <>{children}</>;
     } else {
-      // 无 token 且非白名单路由，重定向至登录页
-      return navigate(loginRoute);
+      navigate(loginRoute);
     }
   }
+
+  return <>{children}</>;
 }
